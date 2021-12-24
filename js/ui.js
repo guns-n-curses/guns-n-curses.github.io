315790190b5c3f556c9cd853a8e1cca9776ed9d5
guns-n-curses <96588649+guns-n-curses@users.noreply.github.com>
2021-12-24 14:09:20 +0100

    crispy

--- js/ui.js
+++ js/ui.js
@@ -26,43 +26,92 @@ ui.switchGender = function () {
 
 
 
+ui.display = new ROT.Display({
+    width: 19,
+    height: 19,
+    fontFamily: "unifont",
+    fontWeight: 100,
+    fontSize: 16,
+    forceSquareRatio: true,
+    bg: "#2b2a33",
+    fg: "#FFFFFF",
+});
+$("#canvas-container").append(ui.display.getContainer());
+
+
+
 ui.init = function () {
-
-    ui.display = new ROT.Display({
-        width: 19,
-        height: 19,
-        fontFamily: "unifont",
-        fontSize: 16,
-        forceSquareRatio: true,
-        bg: "#2b2a33",
-        fg: "#FFFFFFC0",
-    });
-    $("#canvas-container").append(ui.display.getContainer());
-
+    
     ui.splashScreen();
+}
+
+
+
+ui.myDisplay = $("#crispy")[0];
+
+
+
+ui.clearMyDisplay = function() {
+
+    ui.myDisplay.value = "\u00A0".repeat(19*19);
+}
+
+
+
+ui.clearMyDisplay();
+
+
+
+ui.displayLines = Array(19).fill("\u00A0".repeat(19));
+
+
+
+ui.myDrawText = function(x, y, crispyText, blurryText = '') {
+
+    ui.displayLines[y] = ui.displayLines[y].substr(0, x) +
+        crispyText +
+        ui.displayLines[y].substr(x + crispyText.length);
+    ui.myDisplay.value = ui.displayLines.join('\n');
+    
+    ui.display.drawText(x, y, blurryText);
+}
+
+
+
+ui.myDraw = function(x, y, crispyChar, blurryChar = "\u00A0", fg, bg) {
+
+    let p = y * 19 + x;
+    let current = ui.myDisplay.value;
+    current = current.substr(0, p) + crispyChar + current.substr(p + 1);
+    ui.myDisplay.value = current;
+    
+    ui.display.draw(x, y, blurryChar, fg, bg);
 }
 
 
 
 ui.splashScreen = function () {
 
+    ui.clearMyDisplay();
     ui.display.clear();
 
     for (let i = 0; i < 333; i++) {
         let x = Math.floor(9.5 * Math.random() + 9.5 * Math.random());
         let y = 19 - Math.floor((19 * Math.random() * 19 * Math.random()) / 361 * 19);
-        let c = Math.sqrt((9.5-x)*(9.5-x) + y*y) / 19;
-        ui.display.draw(
+        let c = Math.sqrt((9.5 - x) * (9.5 - x) + y * y) / 19;
+        /*ui.display.draw(
             x, y,
             '~', ROT.Color.toHex(ROT.Color.interpolate([255, 0, 0], [127, 127, 0], c)), "#00000010"
-        );
+        );*/
+        ui.myDraw(x, y, ' ', '~', ROT.Color.toHex(ROT.Color.interpolate([255, 0, 0], [127+64, 127+64, 0], c)), "#00000010");
     }
-    ui.display.drawText(3, 8,  "-------------");
-    ui.display.drawText(3, 9,  "GUNS И CURSES");
-    ui.display.drawText(3, 10, "-------------");
+    ui.myDrawText(3, 8,  "-------------", "%c{#2b2a33}-------------");
+    ui.myDrawText(3, 9,  "GUNS И CURSES", "%c{#2b2a33}GUNS И CURSES");
+    ui.myDrawText(3, 10, "-------------", "%c{#2b2a33}-------------");
 }
 
 setInterval(ui.splashScreen, 100);
+
 
 ui.cards = [];
 
@@ -88,7 +137,7 @@ ui.drawCard = function (card) {
 
 
 
-ui.redrawAllCards = function() {
+ui.redrawAllCards = function () {
 
     $(".removable.card").remove();
 
@@ -97,7 +146,7 @@ ui.redrawAllCards = function() {
 
 
 
-ui.checkCharacter = function() {
+ui.checkCharacter = function () {
 
     setTimeout(() => {
         let type = $("input[name='race-radio']:checked").val();
@@ -109,7 +158,7 @@ ui.checkCharacter = function() {
 
 
 
-ui.characterChoiceOk = function() {
+ui.characterChoiceOk = function () {
 
     setTimeout(() => {
         let type = $("input[name='race-radio']:checked").val();
@@ -134,7 +183,7 @@ ui.gameStateUI["home"] = function () {
     <a onclick="manager.newGame()">${"Start a new game"}</a>
     <a onclick="manager.consultBookOfDeath()">${"Consult the book of death"}</a>
     <a onclick="ui.switchLanguage()">${"Change language"}</a>
-    <a onclick="ui.switchGender()">${"Change gender"} (${ui.gender == 'm' ? "male" : "female" })</a>
+    <a onclick="ui.switchGender()">${"Change gender"} (${ui.gender == 'm' ? "male" : "female"})</a>
     `);
 }
 
